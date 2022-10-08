b8ef6a3b756fbf1ce082fe7043f4fb0dee8810d7
Stephen <2stph16@gmail.com>
2022-10-08 13:33:57 -0400

    Added gpt-3 code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config()
 
-import { Configuration, OpenAIApi } from ("openai");
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -25,5 +25,19 @@ const client = new Client(
 client.login(process.env.DISCORD_TOKEN);
 
 client.on("messageCreate", async (message) => {
-    console.log(message)
+    console.log(message.content)
+    const response = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: `Decide whether this message is promoting hate speech.\n\nMessage: \"${message.content}\"\nIs this message promoting hate speech:`,
+        temperature: 0,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+    });
+    const responseText = (response.data.choices[0].text).trim();
+    if (responseText === "Yes") {
+        message.delete();
+    }
+
 })
