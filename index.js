3a1d51f10bf8020b2a15637b7625b78311158526
Stephen <2stph16@gmail.com>
2022-10-08 13:22:47 -0400

    Added discord config

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 import dotenv from 'dotenv';
 dotenv.config()
 
-import { Client } from 'discord.js'
+import { Configuration, OpenAIApi } from ("openai");
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+import { Client, GatewayIntentBits } from 'discord.js'
+
+const client = new Client(
+    {
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+        ]
+    }
+)
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", async (message) => {
+    console.log(message)
+})
