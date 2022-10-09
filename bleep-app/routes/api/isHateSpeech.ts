3b0d03de22f96aa7b2874a7700e5c24282cb9abe
Stephen <2stph16@gmail.com>
2022-10-09 01:15:22 -0400

    added fresh framework site

---
+++ bleep-app/routes/api/isHateSpeech.ts
@@ -0,0 +1,41 @@
+import { HandlerContext } from "$fresh/server.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import "https://deno.land/x/xhr@0.1.0/mod.ts";
+import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.0.0";
+
+const configuration = new Configuration({
+  apiKey: config().OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export const handler = async (
+  _req: Request,
+  _ctx: HandlerContext
+): Response => {
+  if (_req.method === "POST") {
+    const requestBody = await _req.json();
+    const prompt = requestBody.prompt;
+
+    try {
+      const isHateSpeechResponse = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: `Decide whether this message is hate speech.\n\nMessage: \"${prompt}\"\nIs this message hate speech:`,
+        temperature: 0,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+      });
+      const isHateSpeech = isHateSpeechResponse.data.choices[0].text.trim();
+
+      if (isHateSpeech === "Yes") {
+        return new Response("true");
+      }
+
+      return new Response("false");
+    } catch (error) {
+      console.log(error);
+      return new Response("error");
+    }
+  }
+};
