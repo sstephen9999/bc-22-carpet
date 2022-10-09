93b6fb1faace684f1f6327b732eaeff0c128b990
brandonmkim <bran.mkim@gmail.com>
2022-10-09 13:10:15 -0400

    site is finished?

--- bleep-site/components/Navbar.tsx
+++ bleep-site/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useEffect } from 'react';
 
 const Navbar = () => {
     return (
         <div>
             <nav>
                 <div className="logo">
-                    <Image src="/logo.png" alt="bleep.gg" width={655/4} height={188/4}/>
+                    <a href="/">
+                        <Image src="/logo.png" alt="bleep.gg" width={655/4} height={188/4}/>
+                    </a>
                 </div>
-                <Link href="/"><a className="font-switzer">About</a></Link>
-                <Link href="/"><a className="font-switzer">Features</a></Link>
-                <Link href="/"><a className="font-switzer">Get it now</a></Link>
+                <Link href="#about"><a className="font-switzer">About</a></Link>
+                <Link href="#features"><a className="font-switzer">Features</a></Link>
+                <Link href="#getitnow"><a className="font-switzer">Get it now</a></Link>
             </nav>
         </div>
     );
